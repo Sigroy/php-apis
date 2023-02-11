bb123550c3b4e27d66a06f1c41b132a0926c2cb6
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-10 23:28:20 -0600

    start working with curl

---
+++ curl.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+$ch = curl_init();
+
+//curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api");
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$headers = [
+    "Authorization: Client-ID z0rmkzaAxt2xALtk1GL9AL6BusPbRaDzWkE41apl6Jo"
+];
+
+$response_headers = [];
+
+$header_callback = function (CurlHandle $ch, string $header) use (&$response_headers): int {
+
+    $len = mb_strlen($header);
+
+    $parts = explode(":", $header, 2);
+
+    if (count($parts) < 2) {
+        return $len;
+    }
+
+    $response_headers[$parts[0]] = trim($parts[1]);
+
+    return $len;
+
+};
+
+curl_setopt_array($ch, [
+    CURLOPT_URL => "https://api.unsplash.com/photos/random",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => $headers,
+//    CURLOPT_HEADER => true,
+    CURLOPT_HEADERFUNCTION => $header_callback,
+]);
+
+$response = curl_exec($ch);
+
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+//$content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+//$content_length = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+
+curl_close($ch);
+
+echo $status_code . "<br>";
+
+echo '<pre>';
+print_r($response_headers);
+echo '</pre>';
+
+echo $response . "<br>";
