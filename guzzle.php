4486368e69cc209e903cd0d1130353ef95ef2b9f
Sigroy <sigfredoolmedo6c17@gmail.com>
2023-02-18 17:06:41 -0600

    update code to use the randomuser.me api

--- guzzle.php
+++ guzzle.php
@@ -4,15 +4,27 @@ require __DIR__ . '/vendor/autoload.php';
 
 $client = new GuzzleHttp\Client();
 
-$response = $client->request("GET", "https://api.github.com/user/repos", [
+$response = $client->request("GET", "https://randomuser.me/api", [
     "headers" => [
         "Authorization" => 'token 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',
         "User-Agent" => "Sigroy",
     ]
 ]);
 
+/* Magic methods on the client make it easy to send synchronous requests:
+$response = $client->get('http://httpbin.org/get');
+$response = $client->delete('http://httpbin.org/delete');
+$response = $client->head('http://httpbin.org/get');
+$response = $client->options('http://httpbin.org/get');
+$response = $client->patch('http://httpbin.org/patch');
+$response = $client->post('http://httpbin.org/post');
+$response = $client->put('http://httpbin.org/put');
+ */
+
 echo $response->getStatusCode() . "\n";
 
-echo $response->getHeader("content-type")[0], "\n";
+echo $response->getHeader('content-type')[0], "\n";
+//var_dump($response->getHeader('content-type'));
 
-echo substr($response->getBody(), 0, 200), "...\n";
+echo $response->getBody();
+//var_dump($response->getBody());
